e11178a36a3e28c066290d78dabc1b37509240c2
phughesmcr <github@phugh.es>
2025-02-25 16:56:33 +0000

    lint fixes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -86,10 +86,8 @@ export default function App({ Component }: PageProps) {
 
         <link rel="stylesheet" href={asset("/styles.css")} />
 
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: `${JSON.stringify(JSONLD)}` }}
-        >
+        <script type="application/ld+json">
+          {JSON.stringify(JSONLD)}
         </script>
         {
           /* {
